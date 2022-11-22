92aadb5f34596fa72970bbbdddf1485fc181de23
AnKlvy <andreyklimov.ver2@gmail.com>
2022-11-22 15:18:29 +0600

    first map using component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import {ReactComponent as StarWarsLogoSVG} from "./assets/star-wars.svg";
+import {ReactComponent as IconSearchSVG} from "./assets/icon-search.svg";
+import {ReactComponent as FacebooLogokSVG} from "./assets/facebook.svg";
+import {ReactComponent as InstagramLogoSVG} from "./assets/instagram.svg";
+import {ReactComponent as TwitterLogoSVG} from "./assets/twitter.svg";
+import {ReactComponent as YoutubeLogoSVG} from "./assets/youtube.svg";
+
+import swCharacters from "./data/characters.json";
+import swPlanets from "./data/planets.json";
+import swStarships from "./data/starships.json";
+
+const swLinks = [
+{
+url: "https://ru-ru.facebook.com/StarWars/",
+name: "facebook-logo",
+component: <FacebooLogokSVG />
+},
+{
+  url: "https://www.instagram.com/starwars/",
+  name: "instagram-logo",
+  component: <InstagramLogoSVG />
+},
+{
+  url: "https://twitter.com/starwars",
+  name: "twitter-logo",
+  component: <TwitterLogoSVG />
+},
+{
+  url: "https://www.youtube.com/c/StarWars",
+  name: "youtube-logo",
+  component: <YoutubeLogoSVG />
+}
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div className="links-layout">
+      {swLinks.map((link)=>(
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        key={link.name}
+        className="link"
+        href={link.url}
+        target="_blank"
+        alt={link.name}
+        rel="noreferrer">
+        {link.component}
         </a>
-      </header>
+      ))}
     </div>
   );
 }
